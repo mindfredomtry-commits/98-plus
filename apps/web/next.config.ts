8ad067a973d2c425bd6946e2ea1e37355b43d88f
Andrew Brick <mindfre.domtry@gmail.com>
2026-05-22 01:51:56 +0400

    fix vercel shared alias

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,17 @@
+import path from "path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["@98plus/shared"],
+
+  webpack: (config) => {
+    config.resolve.alias["@98plus/shared"] = path.resolve(
+      __dirname,
+      "../../packages/shared/src"
+    );
+
+    return config;
+  },
 };
 
 export default nextConfig;
